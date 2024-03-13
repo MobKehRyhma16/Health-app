dd87e3fb7f773519b81f5e205f7ae1cd2c6a12d9
santtuniskanen <santtuniska02@gmail.com>
2024-03-13 13:21:16 +0200

    added getLocation and function directory

---
+++ functions/getLocation.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import { Platform } from "react-native";
+import Device from "expo-device";
+import * as Location from "expo-location";
+
+export default function getLocation() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS === "android" && !Device.isDevice) {
+        setErrorMsg(
+          "Oops, this will not work on Snack in an Android Emulator. Try it on your device!"
+        );
+        return;
+      }
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+}
