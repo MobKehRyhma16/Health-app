4553bb05dbea66fc868d54dfe1dcdb7cece392b0
Joonatan N <Joona-ni@outlook.com>
2024-03-13 11:22:05 +0200

    Firebase yhdistetty

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import Constants from 'expo-constants';
+import { StyleSheet, Text, SafeAreaView, View} from 'react-native';
+import Login from './Components/Login';
 
 export default function App() {
+
+  const [logged, setLogged] = useState(false);
+
+  if(logged){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+      <Text>You have logged in</Text>
+      </View>
+    </SafeAreaView>
+  );
+} else {
+  return (
+    <Login setLogin={setLogged} />
   );
 }
-
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    paddingTop: Constants.statusBarHeight,
+  }
 });
