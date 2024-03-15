1b3dfdc8b3e074cd7fae489f935a36a08ddd745b
Joonatan N <Joona-ni@outlook.com>
2024-03-15 14:30:21 +0200

    Added logout

--- Components/DrawerComponents/ProfileDrawer.js
+++ Components/DrawerComponents/ProfileDrawer.js
@@ -2,21 +2,48 @@ import { createDrawerNavigator, DrawerToggleButton } from '@react-navigation/dra
 import ProfileScreen from '../../Screens/ProfileScreen';
 import LoginScreen from '../../Screens/LoginScreen';
 import Info from './Info';
+import Logout from './Logout';
 import DrawerStyles from './DrawerStyles';
+import { getAuth } from '../../Firebase/Config';
+import { useEffect, useState } from 'react';
 
 const Drawer = createDrawerNavigator();
+const auth = getAuth();
 
 function ProfileDrawer() {
+  const [user, setUser] = useState(null);
+  const [initializing, setInitializing] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      setUser(user);
+      if (initializing) {
+        setInitializing(false);
+      }
+    });
+
+    return unsubscribe;
+  }, [initializing]);
+
+  if (initializing) {
+    return null;
+  }
+
   return (
-    <Drawer.Navigator 
-    screenOptions={{
-      drawerPosition: 'right',
-      headerLeft: false,
-      headerRight: () => <DrawerToggleButton />,
-    drawerStyle: DrawerStyles.drawerContainer}}
+    <Drawer.Navigator
+      screenOptions={{
+        drawerPosition: 'right',
+        headerLeft: false,
+        headerRight: () => <DrawerToggleButton />,
+        drawerStyle: DrawerStyles.drawerContainer
+      }}
     >
       <Drawer.Screen name="Profile" component={ProfileScreen} />
-      <Drawer.Screen name="Login" component={LoginScreen} />
+      {user ? (
+        <Drawer.Screen name="Logout" component={Logout} />
+      ) : (
+        <Drawer.Screen name="Login" component={LoginScreen} />
+      )}
       <Drawer.Screen name="My Information" component={Info} />
     </Drawer.Navigator>
   );
