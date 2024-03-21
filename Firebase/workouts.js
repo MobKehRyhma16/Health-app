9a11b75656c3439b187e63904ba16015281eaa67
unknown <joonasroska123@gmail.com>
2024-03-21 19:03:46 +0200

    workouts fetch and save works

--- Firebase/workouts.js
+++ Firebase/workouts.js
@@ -1,7 +1,37 @@
-import { firestore, collection, query, where, onSnapshot } from "./Config";
+import { firestore, collection, query, where, onSnapshot, WORKOUTS } from "./Config";
 import { convertFirebaseTimeStampToJS } from "../helpers/Functions";
 import { useEffect, useState } from "react";
-import { doc } from 'firebase/firestore'; // Import the 'doc' function
+import { addDoc, doc } from 'firebase/firestore'; // Import the 'doc' function
+
+export const saveWorkout  = async (userid ,calories, steps, duration, workout_type) => {
+    // const [calories, setCalories]
+
+    try{
+        const docRef = await addDoc(collection(firestore,WORKOUTS), {
+            calories: calories,
+            steps: steps,
+            duration: duration,
+            //todo check the types of route and created at
+            created_at: new Date(),
+            // todo Nested arrays not allowed
+            // route: [[10,11],[12,13]],
+            user_id: `/users/${userid}`,
+            workout_type: workout_type
+    })
+    } catch(error){
+        console.log(error)
+    }
+
+    // id: doc.id,
+    // calories: doc.data().calories,
+    // created: convertFirebaseTimeStampToJS(doc.data().created_at),
+    // duration: doc.data().duration,
+    // user_id: doc.data().user_id.id,
+    // steps: doc.data().steps,
+    // workout_type: doc.data().workout_type,
+}
+ 
+
 
 export const getWorkouts = (userId) => {
     const [workouts, setWorkouts] = useState([]);
--- Screens/HistoryScreen.js
+++ Screens/HistoryScreen.js
@@ -1,52 +1,65 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, TextInput, Button, StyleSheet, SafeAreaView } from "react-native";
-// import { firestore, collection, addDoc, serverTimestamp, query, onSnapshot , WORKOUTS } from "../Firebase/Config";
-// import { convertFirebaseTimeStampToJS } from "../helpers/Functions";
-import { getWorkouts } from "../Firebase/getWorkouts";
-
-// import GetWorkout from "../Components/Workouts/GetWorkout";
+import { View, Text, StyleSheet, SafeAreaView, Button } from "react-native";
+import { getWorkouts, saveWorkout } from "../Firebase/workouts";
 
 export default function HistoryScreen() {
+    const userId = 'VlxwyuiQTxRE1w5eii4kcReqhTU2'; // user id for testing
 
+    const workouts = getWorkouts(userId);
 
-    const workouts = getWorkouts('VlxwyuiQTxRE1w5eii4kcReqhTU2')
-
-    //For debugging
     useEffect(() => {
-    console.log('WORKOUTS:: ', workouts);
+        console.log('WORKOUTS FOR USER_ID ' + userId + ': ', workouts);
 
-    workouts.forEach(workout => {
-      console.log(workout.steps);
-    });
+        workouts.forEach(workout => {
+            console.log(workout.steps);
+        });
     }, [workouts]);
 
-    useEffect(() => {
-      
-    }, []);
-
-
     return (
-        <SafeAreaView style={styles.container}>
+        <SafeAreaView style={styles.container}>               
+          <Button title="TEST SAVE" onPress={() => saveWorkout(userId,101,201,3000,'running')}></Button>
+
             <View>
                 <Text style={styles.heading}>History</Text>
-
-                {/* {workouts.map((workout) => {
-                    <Text>{workout}</Text>
-                } )} */}
+                {workouts.length > 0 ? (
+                    workouts.map((workout, index) => (
+                        <WorkoutItem key={index} workout={workout} />
+                    ))
+                ) : (
+                    <Text>No workouts available</Text>
+                )}
             </View>
         </SafeAreaView>
     );
 }
 
+const WorkoutItem = ({ workout }) => {
+    return (
+        <View style={styles.workoutItem}>
+            <Text>Steps: {workout.steps}</Text>
+            <Text>Calories: {workout.calories}</Text>
+            <Text>Duration: {(workout.duration / 60).toFixed()} minutes</Text>
+        </View>
+    );
+};
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
+        justifyContent: "center",
+        alignItems: "center",
         padding: 16,
     },
     heading: {
         fontSize: 24,
-        fontWeight: 'bold',
+        fontWeight: "bold",
         marginBottom: 16,
+    },
+    workoutItem: {
+        borderWidth: 1,
+        borderColor: "#ccc",
+        borderRadius: 8,
+        padding: 16,
+        marginBottom: 12,
     },
 });
