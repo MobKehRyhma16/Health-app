735eca8c670f5ffc5ab55b0792ea180858e6bce3
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2024-04-04 12:11:26 +0300

    bottomActions in ongoing workout proggress

--- Screens/OngoingWorkoutScreen.js
+++ Screens/OngoingWorkoutScreen.js
@@ -1,22 +1,75 @@
-import { View, Text } from 'react-native'
-import React, { useEffect } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { Button, IconButton } from 'react-native-paper';
+
 
 
 const OngoingWorkoutScreen = () => {
+
+  const [visible,setVisible] = useState('false')
 
     useEffect(() => {
         console.log('In ongoig workoutscreen!')
     }, []);
 
+
+  const BottomActions = () => {
+    return (
+      <>
+        <Button textColor='red' onPress={() => console.log('Quit button pressed')} icon="cancel">End Workout</Button>
+        <IconButton size={30} mode='contained' icon='chevron-up'></IconButton>
+        {/* <Button style={styles.expandButton} mode='elevated' icon='chevron-up'></Button> */}
+        <Button onPress={() => console.log('Pause button pressed')} icon="pause-circle-outline">Pause Workout </Button>
+      </>
+      );
+  }
+   
     
 
   return (
-    <SafeAreaView>
-      <Text>OngoingWorkoutScreen, type:</Text>
+    <SafeAreaView style={styles.container}>
+
+
+      <Text style={styles.textCont}>OngoingWorkoutScreen, type:</Text>
+
+      <View style={styles.bottomContainer}>
+        <BottomActions/>
+      </View>
+        
 
     </SafeAreaView>
   )
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      
+
+      
+  },
+
+  textCont: {
+    flex: 1,
+    textAlign: 'center'
+  },
+
+  bottomContainer: {
+    flex: 0.1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    // backgroundColor: '#FFAAAA',
+    
+
+    // justifyContent: 'center'
+  },
+  // expandButton: {
+  //   justifyContent: 'space-around'
+  // }
+
+});
 
 export default OngoingWorkoutScreen
