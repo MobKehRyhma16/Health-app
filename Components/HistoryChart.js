d1da0bdb5315bd2a63f104b007c2ed9865fcd6ad
unknown <joonasroska123@gmail.com>
2024-04-17 17:19:16 +0300

    chart type switching works

--- Components/HistoryChart.js
+++ Components/HistoryChart.js
@@ -4,52 +4,69 @@ import { BarChart } from 'react-native-gifted-charts';
 import { ToggleButton } from 'react-native-paper';
 import { parseDurationToSeconds } from '../helpers/Functions';
 
-const HistoryChart = ({ workouts }) => {
+export const HistoryChart = ({ workouts }) => {
   const [type, setType] = useState('duration');
 
   const dataDistance = workouts.map(workout => ({ value: workout.distance }));
-  const dataDuration = workouts.map(workout => ({ value: parseDurationToSeconds(workout.duration) }));
+  const dataDuration = workouts.map(workout => ({ value: parseDurationToSeconds(workout.duration)/60 }));
   
   const updateType = inputType => {
     setType(inputType);
   };
 
+  const DurationChart = () => {
+
+    return (
+      <BarChart
+      style={styles.chart}
+      barWidth={50}
+      noOfSections={3}
+      barBorderRadius={4}
+      frontColor="lightcoral"
+      data={dataDuration}
+      yAxisThickness={0}
+      xAxisThickness={0}
+    />
+    );
+  }
+
+  const DistanceChart = () => {
+
+    return (
+      <BarChart
+      style={styles.chart}
+      barWidth={50}
+      noOfSections={3}
+      barBorderRadius={4}
+      frontColor="lightcoral"
+      data={dataDistance}
+      yAxisThickness={0}
+      xAxisThickness={0}
+    />
+    );
+  }
+   
+
+
   return (
     <View style={styles.chartContainer}>
       <ToggleSwitch updateType={updateType} />
       {type === 'duration' ? (
-        <BarChart
-          style={styles.chart}
-          barWidth={50}
-          noOfSections={3}
-          barBorderRadius={4}
-          frontColor="lightcoral"
-          data={dataDuration}
-          yAxisThickness={0}
-          xAxisThickness={0}
-        />
+        <DurationChart></DurationChart>
       ) : (
-        <BarChart
-          style={styles.chart}
-          barWidth={50}
-          noOfSections={3}
-          barBorderRadius={4}
-          frontColor="lightcoral"
-          data={dataDistance}
-          yAxisThickness={0}
-          xAxisThickness={0}
-        />
+        <DistanceChart></DistanceChart>
       )}
     </View>
   );
 };
 
 const ToggleSwitch = ({ updateType }) => {
-  const [value, setValue] = React.useState('');
+  const [value, setValue] = React.useState('duration'); // Initialize value
 
   useEffect(() => {
     updateType(value);
   }, [value]);
+
 
   return (
     <ToggleButton.Row onValueChange={value => setValue(value)} value={value}>
